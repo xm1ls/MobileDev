7fdd5f9ae2f936870c42e50b67b9ccbca8595bd1
xm1ls <xm1ls2000@gmail.com>
2024-02-11 17:28:37 +0200

    add label component

--- app/src/main/java/com/example/settingspage/MainActivity.kt
+++ app/src/main/java/com/example/settingspage/MainActivity.kt
@@ -6,23 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.settingspage.ui.theme.SettingsPageTheme
+import com.example.settingspage.ui.theme.robotoFontFamily
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,18 +42,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SettingsPage(modifier: Modifier = Modifier) {
-    Profile(
-        image = painterResource(id = R.drawable.img_avatar),
-        imageHorizontalArrangement = Arrangement.End,
-        imageModifier = Modifier
-            .clip(RoundedCornerShape(50))
-            .size(51.dp)
-        ,
-        bgModifier = Modifier
-            .height(100.dp)
-            .background(color = MaterialTheme.colorScheme.background)
-            .padding(horizontal = 21.dp, vertical = 15.dp)
-    )
+    Column(
+
+    ) {
+        Profile(
+            image = painterResource(id = R.drawable.img_avatar),
+            imageHorizontalArrangement = Arrangement.End,
+            imageModifier = Modifier
+                .clip(RoundedCornerShape(50))
+                .size(51.dp)
+            ,
+            bgModifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.background)
+                .padding(horizontal = 21.dp, vertical = 15.dp)
+        )
+        Label(
+            text = "Settings",
+            fontFamily = robotoFontFamily,
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Normal,
+            textModifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.background)
+                .padding(start = 21.dp, top = 0.dp, end = 21.dp, bottom = 23.dp)
+        )
+    }
 
 }
 
@@ -69,10 +88,32 @@ fun Profile(
             painter = image,
             contentDescription = "Profile Image",
             modifier = imageModifier
-
         )
     }
 }
+
+@Composable
+fun Label(
+    text: String,
+    fontFamily: FontFamily,
+    fontSize: TextUnit,
+    fontWeight: FontWeight,
+    textModifier: Modifier = Modifier,
+    bgModifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = bgModifier
+    ) {
+        Text(
+            text = text,
+            fontFamily = fontFamily,
+            fontSize = fontSize,
+            fontWeight = fontWeight,
+            modifier = textModifier
+        )
+    }
+}
+
 
 @Preview(
     showBackground = true,
